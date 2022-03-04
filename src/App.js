d473b3208a46c5a27d8895d81db935e64476735b
Jacob Veasey <jbveas01@gmail.com>
2022-03-04 12:58:17 -0500

    Added fields to form with proper handling. Created probe object with form data

--- src/App.js
+++ src/App.js
@@ -3,27 +3,49 @@ import ProbeForm from "./components/ProbeForm";
 
 function App() {
   const [newSerial, setSerial] = useState("");
+  const [newCert, setCert] = useState('')
+  const [newLot, setLot] = useState('')
+  const [newManuf, setManuf] = useState('')
+  const [probeList, setProbeList] = useState([])
 
-  ///Handle serial number change
-  const handleSerialChange = (event) => {
-    setSerial(event.target.value);
-  };
+  ///Handle Form changes
+  const handleSerialChange = (event) => setSerial(event.target.value)
+  const handleCertChange = (event) => setCert(event.target.value)
+  const handleLotChange = (event) => setLot(event.target.value)
+  const handleManufChange = (event) => setManuf(event.target.value)
+
   ///Handles Submitting the probe into the database
   const submitProbe = (event) => {
     event.preventDefault();
     const data = new FormData(event.target)
-    console.log(data.get('serial'))
-    console.log(data.get('certDate'))
+    const probe = {
+      serial: data.get('serial'),
+      certificationDate: new Date(data.get('certDate')),
+      lotNum: data.get('lot#'),
+      manufactureDate: new Date(data.get('manufDate')),
+      ///Add 2 years to certifcation date for expiration date
+      expirationDate: new Date(new Date(data.get('certDate')).setFullYear((new Date(data.get('certDate')).getFullYear()) + 2))
+    }
+    const probes = probeList.concat(probe)
+    setProbeList(probes)
     setSerial("");
+    setCert("")
+    setLot('')
+    setManuf('')
   };
 
   return (
     <div className="App">
       <ProbeForm
         newSerial={newSerial}
-        onChange={handleSerialChange}
+        newCert={newCert}
+        newLot={newLot}
+        newManuf={newManuf}
+        handleLotChange={handleLotChange}
+        handleCertChange={handleCertChange}
+        handleSerialChange={handleSerialChange}
+        handleManufChange={handleManufChange}
         onSubmit={submitProbe}
-        placeHolder='Enter Probe Serial#'
       />
     </div>
   );
--- src/components/ProbeForm.js
+++ src/components/ProbeForm.js
@@ -1,11 +1,11 @@
-const ProbeForm = ({ newSerial, onChange, onSubmit, placeHolder }) => {
+const ProbeForm = ({ newCert, newSerial, newLot, newManuf, handleManufChange, handleSerialChange, onSubmit, handleCertChange, handleLotChange }) => {
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input name='serial' value={newSerial} onChange={onChange} placeholder={placeHolder} />
-        <input name='certDate' placeholder='Enter certification date' />
-        <input name='lot#' placeholder='Enter Lot #' />
-        <input name='manufDate' placeholder='Enter manufactured date' />
+        <input name='serial' value={newSerial} onChange={handleSerialChange} placeholder='Enter Probe Serial #' />
+        <input name='certDate' value={newCert} onChange={handleCertChange} placeholder='Enter certification date' />
+        <input name='lot#' value={newLot} onChange={handleLotChange} placeholder='Enter Lot #' />
+        <input name='manufDate' value={newManuf} onChange={handleManufChange} placeholder='Enter manufactured date' />
         <button type="submit">Submit data</button>
       </form>
     </div>
