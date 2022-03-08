012b6810fa8f72b06eb67c48786cc65243e85c26
Jacob Veasey <Jbveas01@gmail.com>
2022-03-07 20:43:22 -0500

    Added useEffect hook to refresh page. Added delete functionality. Page now updates with current DB data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ProbeForm from "./components/ProbeForm";
 import Dashboard from "./views/Dashboard";
 import Navbar from "./components/Navbar";
 import axios from "axios";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   const [newSerial, setSerial] = useState("");
@@ -11,15 +11,17 @@ function App() {
   const [newLot, setLot] = useState("");
   const [newManuf, setManuf] = useState("");
   const [filter, setFilter] = useState("");
-  const [probeList, setProbeList] = useState([
-    {
-      _id: "GP00001",
-      certificationDate: "03/04/2022",
-      lotNum: "LXW21",
-      manufactureDate: "02/02/2022",
-      expirationDate: "03/04/2024",
-    },
-  ]);
+  const [useTrigger, setTrigger] = useState(false);
+  const [probeList, setProbeList] = useState([]);
+  const [deleteProbe, setDeleteProbe] = useState("");
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/probes").then((res) => {
+      console.log("Probes Grabbed");
+      console.log(res.data);
+      setProbeList(res.data);
+    });
+  }, [useTrigger]);
 
   ///Handle Form changes
   const handleSerialChange = (event) => setSerial(event.target.value);
@@ -28,21 +30,9 @@ function App() {
   const handleManufChange = (event) => setManuf(event.target.value);
 
   ///Handles Submitting the probe into the database
-  const submitProbe = (event) => {
+  const submitProbe = async (event) => {
     event.preventDefault();
     const data = new FormData(event.target);
-    // const probe = {
-    //   serial: data.get("serial"),
-    //   certificationDate: new Date(data.get("certDate")),
-    //   lotNum: data.get("lot#"),
-    //   manufactureDate: new Date(data.get("manufDate")),
-    //   ///Add 2 years to certifcation date for expiration date
-    //   expirationDate: new Date(
-    //     new Date(data.get("certDate")).setFullYear(
-    //       new Date(data.get("certDate")).getFullYear() + 2
-    //     )
-    //   ),
-    // };
     const probe = {
       _id: data.get("serial"),
       certificationDate: new Date(data.get("certDate")),
@@ -55,44 +45,71 @@ function App() {
         )
       ),
     };
-    const probes = probeList.concat(probe);
-    setProbeList(probes);
     setSerial("");
     setCert("");
     setLot("");
     setManuf("");
-    axios.post("http://localhost:3001/api/probes", probe).then((res) => {
+    await axios.post("http://localhost:3001/api/probes", probe).then((res) => {
       console.log(res);
     });
+    setTrigger(!useTrigger);
   };
 
   const filterProbes = () => {
-    const filteredProbes = probeList.filter(probe => probe._id.includes(filter))
-    console.log(filteredProbes)
-  }
+    const filteredProbes = probeList.filter((probe) =>
+      probe._id.includes(filter)
+    );
+    console.log(filteredProbes);
+  };
   const changeFilter = (event) => {
-    setFilter(event.target.value)
-  }
+    setFilter(event.target.value);
+  };
+  const changeDelete = (event) => {
+    setDeleteProbe(event.target.value);
+  };
+
+  const handleDelete = async (event) => {
+    event.preventDefault();
+    await axios
+      .delete(`http://localhost:3001/api/probes/${deleteProbe}`)
+      .then((res) => {
+        console.log(res.data);
+      });
+    setDeleteProbe("");
+    setTrigger(!useTrigger);
+  };
 
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path='/probes' element={<ProbeForm
-          newSerial={newSerial}
-          newCert={newCert}
-          newLot={newLot}
-          newManuf={newManuf}
-          handleLotChange={handleLotChange}
-          handleCertChange={handleCertChange}
-          handleSerialChange={handleSerialChange}
-          handleManufChange={handleManufChange}
-          onSubmit={submitProbe}
-        />}>
-        </Route>
+        <Route
+          path="/probes"
+          element={
+            <ProbeForm
+              newSerial={newSerial}
+              newCert={newCert}
+              newLot={newLot}
+              newManuf={newManuf}
+              handleLotChange={handleLotChange}
+              handleCertChange={handleCertChange}
+              handleSerialChange={handleSerialChange}
+              handleManufChange={handleManufChange}
+              onSubmit={submitProbe}
+            />
+          }
+        ></Route>
       </Routes>
-      <Dashboard probeList={probeList} filter={filter} handleChange={changeFilter} />
-    </div >
+      <Dashboard
+        probeList={probeList}
+        filter={filter}
+        handleChange={changeFilter}
+      />
+      <form onSubmit={handleDelete}>
+        <input value={deleteProbe} onChange={changeDelete}></input>
+        <button type="submit">Delete probe</button>
+      </form>
+    </div>
   );
 }
 
